668fb1aee0efd0fdc76f59c4f6ccca4b44c30970
Mathis <mathis.bremont@etu.unilim.fr>
2020-02-11 08:29:59 +0100

    Page résultats avec ptit bug requete sql

--- models/resultat.js
+++ models/resultat.js
@@ -6,14 +6,21 @@ module.exports.getDerniersResultats = function (callback) {
         if (!err) {
             // s'il n'y a pas d'erreur de connexion
             // execution de la requête SQL
-            let sql = "SELECT gpnom, DAY(gpdate) as daydate, MONTH(gpdate) as monthdate, YEAR(gpdate) as yeardate, " +
+            let sql = "SELECT gpnum, payadrdrap, gpnom, DAY(gpdate) as daydate, MONTH(gpdate) as monthdate, YEAR(gpdate) as yeardate, " +
                 "DAY(gpdatemaj) as daymaj, MONTH(gpdatemaj) as monthmaj, YEAR(gpdatemaj) as yearmaj" +
-                " FROM grandprix where gpdatemaj = (select max(gpdatemaj) from grandprix)";
-            //console.log (sql);
+                " FROM grandprix g " +
+                "INNER JOIN circuit c ON g.cirnum=c.cirnum " +
+                "INNER JOIN pays p ON p.paynum=c.paynum " +
+                "where gpdatemaj = (select max(gpdatemaj) from grandprix)" +
+                "ORDER BY gpnom";
+            console.log (sql);
             connexion.query(sql, callback);
 
             // la connexion retourne dans le pool
             connexion.release();
         }
     });
+
+
+
 };
