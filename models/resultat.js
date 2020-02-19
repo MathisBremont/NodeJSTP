277b8c172bd183e8ac96a1d1d4bfff9fc21421e7
Gautier Mechaussie <49200160+Tyegoteks@users.noreply.github.com>
2020-02-19 19:01:38 +0100

    detail sur page 2 et 3 fait

--- models/resultat.js
+++ models/resultat.js
@@ -6,14 +6,8 @@ module.exports.getDerniersResultats = function (callback) {
         if (!err) {
             // s'il n'y a pas d'erreur de connexion
             // execution de la requête SQL
-            let sql = "SELECT gpnum, payadrdrap, gpnom, DAY(gpdate) as daydate, MONTH(gpdate) as monthdate, YEAR(gpdate) as yeardate, " +
-                "DAY(gpdatemaj) as daymaj, MONTH(gpdatemaj) as monthmaj, YEAR(gpdatemaj) as yearmaj" +
-                " FROM grandprix g " +
-                "INNER JOIN circuit c ON g.cirnum=c.cirnum " +
-                "INNER JOIN pays p ON p.paynum=c.paynum " +
-                "where gpdatemaj = (select max(gpdatemaj) from grandprix)" +
-                "ORDER BY gpnom";
-            console.log (sql);
+            let sql = "SELECT gpnum, gpnom, gpdate, gpdatemaj, payadrdrap from grandprix g inner join circuit c on c.cirnum=g.cirnum inner join pays pa on pa.paynum = c.paynum where gpdatemaj = (select max(gpdatemaj) from grandprix) ORDER BY gpnom";
+
             connexion.query(sql, callback);
 
             // la connexion retourne dans le pool
